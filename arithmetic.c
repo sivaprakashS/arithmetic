579e127fda059b18ec4d7f0cea30e2f893d429a8
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-02 16:27:47 +0530

    arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
-void main()
+int main(void)
 {
-int n,a,d;
-scanf("%d %d %d",&n &a &d);
-int d;
-d=n+a+d;
-printf("%d",&d);
+int a,d,n,i,tn;
+  int sum=0;
+   printf("Enter the first number of the  series: ");
+  scanf("%d",&a);
+    printf("Enter the total numbers in the series: ");
+    scanf("%d",&n);
+    printf("Enter the common difference of  series: ");
+  scanf("%d",&d);
+    sum = ( n * ( 2 * a + ( n -1 ) * d ) )/ 2;
+    tn = a + (n-1) * d;
+     printf("Sum of the series ");
+   for(i=a;i<=tn; i= i + d )
+ {
+	if (i != tn)
+ printf("%d + ",i);
+             else
+  printf("%d = %d ",i,sum);
+  }
+   return 0
 }
